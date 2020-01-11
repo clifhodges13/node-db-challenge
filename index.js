495e65109b7c7d0c280cb2fe2d93dc75035e55f9
Clif <clifhodges13@gmail.com>
2020-01-11 12:43:58 -0600

    create get and post endpoints for tasks table. MVP complete.

--- index.js
+++ index.js
@@ -51,4 +51,36 @@ server.post('/resources', async (req, res) => {
   }
 })
 
+// Get all tasks
+server.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await db('Tasks')
+    res.status(200).json(tasks.map(task => {
+      return {
+        id: task.id,
+        Description: task.Description,
+        Notes: task.Notes,
+        Project_Id: task.Project_Id,
+        Completed: `${task.Completed === 0 ? 'false': 'true'}`
+      }
+    }))
+  } catch(err) {
+    res.status(500).json({message: 'Server error.'})
+  }
+})
+
+// Post a new task
+server.post('/tasks', async (req, res) => {
+  if (!req.body.description) {
+    res.status(400).json({ message: 'You must include a task description in your post request body.' })
+  } else if (!req.body.project_id) {
+    res.status(400).json({ message: 'You must include the project id associated with your task in your post request body.' })
+  }
+  try {
+    res.status(201).json(await db('Tasks').insert(req.body))
+  } catch(err) {
+    res.status(500).json({message: 'Server error.'})
+  }
+})
+
 server.listen(PORT, () => console.log(`Server is listening at ${HOST}:${PORT}`));
